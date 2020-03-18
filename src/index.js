6a90fd63aa8c2a9e8c8b0b98ce10eec858b8f4bf
ilichka <60748637+ilichka@users.noreply.github.com>
2020-03-18 15:29:33 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const openCharStr = bracketsConfig.map(val => val[0]).join('');
+    const closeCharStr = bracketsConfig.map(val => val[1]).join('');
+    let newArr = [];
+    for (let i of str) {
+        if (openCharStr.includes(i) && !closeCharStr.includes(i)) {
+            newArr.push(i);
+        } else if (closeCharStr.includes(i) && !openCharStr.includes(i)) {
+            let openIndex = closeCharStr.indexOf(i);
+            if (openCharStr[openIndex] === newArr[newArr.length - 1]) {
+                newArr.pop();
+            } else return false;
+        } else if (openCharStr.includes(i) && closeCharStr.includes(i)) {
+            if (i === newArr[newArr.length - 1]) {
+                newArr.pop();
+            } else
+                newArr.push(i);
+        }
+    }
+    return newArr.length === 0;
+};
